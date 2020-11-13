b2894857647fd6bdd17b2549722f43afb29f5790
Filip <floffler@wp.pl>
2020-11-13 18:38:11 +0100

    added retrieving files and getting 2 newest sermons

--- src/main/java/com/nellvin/kechservice/controler/SermonControler.java
+++ src/main/java/com/nellvin/kechservice/controler/SermonControler.java
@@ -1,5 +1,6 @@
 package com.nellvin.kechservice.controler;
 
+import com.nellvin.kechservice.model.Post;
 import com.nellvin.kechservice.model.Sermon;
 import com.nellvin.kechservice.service.SermonService;
 import com.nellvin.kechservice.utils.FileUploadUtil;
@@ -13,6 +14,9 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -34,18 +38,18 @@ public class SermonControler {
 
     @CrossOrigin
     @GetMapping("/api/sermons")
-    public List<Sermon> getAllSermons(){
+    public List<Sermon> getAllSermons() {
         return sermonService.retrieveSermons();
     }
 
     @GetMapping("/api/sermons/{id}")
-    public Sermon getSermonById(@PathVariable(value = "id") Long sermonId){
+    public Sermon getSermonById(@PathVariable(value = "id") Long sermonId) {
         return sermonService.getSermon(sermonId);
     }
 
     @GetMapping("/api/sermon/{id}/image")
     public ResponseEntity<Resource> getImage(@PathVariable(value = "id") Long sermonId) throws FileNotFoundException {
-        File file = new File(String.valueOf(Paths.get("sermon-photo/" + sermonId +"/"+sermonService.getSermon(sermonId).getPhoto())));
+        File file = new File(String.valueOf(Paths.get("sermon-photo/" + sermonId + "/" + sermonService.getSermon(sermonId).getPhoto())));
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=image.jpg");
 
@@ -75,7 +79,7 @@ public class SermonControler {
 
     @GetMapping("/api/sermon/{id}/audio")
     public ResponseEntity<Resource> getSermonAudio(@PathVariable(value = "id") Long sermonId) throws FileNotFoundException {
-        File file = new File(String.valueOf(Paths.get("sermon-audio/" + sermonId +"/"+sermonService.getSermon(sermonId).getAudio())));
+        File file = new File(String.valueOf(Paths.get("sermon-audio/" + sermonId + "/" + sermonService.getSermon(sermonId).getAudio())));
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=audio.mp3");
 
@@ -88,7 +92,7 @@ public class SermonControler {
     }
 
     @PostMapping("/api/sermons")
-    public void saveSermon(@RequestBody Sermon sermon){
+    public void saveSermon(@RequestBody Sermon sermon) {
         sermonService.saveSermon(sermon);
         System.out.println("Sermon saved!");
     }
@@ -104,31 +108,48 @@ public class SermonControler {
         sermon.setAudio(audioName);
 
         Sermon savedSermon = sermonService.saveSermon(sermon);
-        sermon.setUrl("http://localhost:8080/api/sermon/"+savedSermon.getId()+"/audio");
+        sermon.setUrl("http://localhost:8080/api/sermon/" + savedSermon.getId() + "/audio");
         sermonService.updateSermon(sermon);
-
 
 
         String uploadPhotoDir = "sermon-photo/" + savedSermon.getId();
         String uploadAudioDir = "sermon-audio/" + savedSermon.getId();
 
-
         FileUploadUtil.saveFile(uploadPhotoDir, photoName, multipartFile1);
         FileUploadUtil.saveFile(uploadAudioDir, audioName, multipartFile2);
+
+        BufferedImage originalImage = ImageIO.read(new File(String.valueOf(Paths.get("sermon-photo/" + savedSermon.getId() + "/" + photoName))));
+        int targetWidth = 50;
+        int targetHeight = originalImage.getHeight() / (originalImage.getWidth() / targetWidth);
+
+        BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics2D = resizedImage.createGraphics();
+        graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
+        graphics2D.dispose();
+
+        FileUploadUtil.saveFile(uploadPhotoDir, photoName.replace(".jpg", "_small.jpg"), resizedImage);
 
 //        return new RedirectView("/users", true);
     }
 
+    @GetMapping("/api/sermons/main")
+    public List<Sermon> getTwoSermons() {
+        List<Sermon> list = sermonService.retrieveSermons();
+        int minIndex = Math.min(0, list.size());
+        int maxIndex = Math.min(2, list.size());
+        return list.subList(minIndex, maxIndex);
+    }
+
     @DeleteMapping("/api/sermons/{id}")
-    public void deleteSermon(@PathVariable(name="id")Long sermonId){
+    public void deleteSermon(@PathVariable(name = "id") Long sermonId) {
         sermonService.deleteSermon(sermonId);
-        System.out.println("Sermon "+ sermonId+" has been deleted");
+        System.out.println("Sermon " + sermonId + " has been deleted");
     }
 
     @PutMapping("/api/sermons/{id}")
-    public void updateSermon(@RequestBody Sermon sermon, @PathVariable(name = "id")Long sermonId){
+    public void updateSermon(@RequestBody Sermon sermon, @PathVariable(name = "id") Long sermonId) {
         Sermon serm = sermonService.getSermon(sermonId);
-        if(serm != null)
+        if (serm != null)
             sermonService.updateSermon(sermon);
 
     }
